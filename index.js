cfdea241f04ca26c5e53be1b098428e24ce200b4
NikhilKansotia <kansotianikhil92@gmail.com>
2025-04-02 22:28:24 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const age = 21;
+const name = "Nikhil Kansotia";
+const dob = "2001-01-01";
+const city = "New Delhi";
+const country = "India";
+const email = "nikhil@gmail.com";
+const phone = "9876543210";
+const gender = "Male";
